ca46ad1375361f7a3ee9f3186090df00ffc22dec
azizhossain <azizmh901@gmail.com>
2021-09-25 21:05:04 +0600

    Updated

--- application/views/front/home.php
+++ application/views/front/home.php
@@ -1,3 +1,4 @@
+<!-- image slider start -->
 <div id="slides" class="carousel slide carousel-cus" data-ride="carousel">
     <ul class="carousel-indicators">
         <li data-target="#slides" data-slide-to="0" class="active"></li>
@@ -6,13 +7,32 @@
     </ul>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="<?php echo base_url().'public/uploads/slide/banner1.jpg';?>">
+            <img src="<?php echo base_url().'public/uploads/slide/Banner1.jpg';?>">
+            <div class="carousel-caption text-right">
+                <h1 class="display-2">Are You Hungry?!</h1>
+                <h3>Don't worry, we are here to serve you</h3>
+                <a href="" class="btn btn-outline-light btn-lg">Order Now</a>
+                <a href="" class="btn btn-primary btn-lg">View Menu</a>
+            </div>
         </div>
         <div class="carousel-item">
-            <img src="<?php echo base_url().'public/uploads/slide/banner2.jpg';?>">
+            <img src="<?php echo base_url().'public/uploads/slide/Banner2.jpg';?>">
+            <div class="carousel-caption text-right">
+                <h1 class="display-2">Are You Hungry?!</h1>
+                <h3>Don't worry, we are here to serve you</h3>
+                <a href="" class="btn btn-outline-light btn-lg">Order Now</a>
+                <a href="" class="btn btn-primary btn-lg">View Menu</a>
+            </div>
         </div>
         <div class="carousel-item">
-            <img src="<?php echo base_url().'public/uploads/slide/banner3.jpg';?>">
+            <img src="<?php echo base_url().'public/uploads/slide/Banner3.jpg';?>">
+            <div class="carousel-caption text-left">
+                <h1 class="display-2">Are You Hungry?!</h1>
+                <h3>Don't worry, we are here to serve you</h3>
+                <a href="" class="btn btn-outline-light btn-lg">Order Now</a>
+                <a href="" class="btn btn-primary btn-lg">View Menu</a>
+            </div>
         </div>
     </div>
 </div>
+<!-- image slider end -->
