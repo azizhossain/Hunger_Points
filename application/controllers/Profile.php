df78af2f94fc32d9be9e547d32bec1db191f1053
azizhossain <mhossainaziz100@gmail.com>
2022-01-25 20:52:33 +0600

    Update User Profile

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -23,4 +23,43 @@ class Profile extends CI_Controller {
         $this->load->view('front/partials/header');
         $this->load->view('front/profile',$data);
     }
+
+       public function edit($id) {
+        $user = $this->User_model->getUser($id);
+
+        if(empty($user)) {
+            $this->session->set_flashdata('error', 'User not found');
+            redirect(base_url().'profile');
+        }
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_error_delimiters('<p class="invalid-feedback">','</p>');
+        $this->form_validation->set_rules('usersname', 'Usersname','trim|required');
+        $this->form_validation->set_rules('firstname', 'First Name','trim|required');
+        $this->form_validation->set_rules('lastname', 'Last Name','trim|required');
+        $this->form_validation->set_rules('email', 'Email','trim|required');
+        $this->form_validation->set_rules('phone', 'Phone','trim|required');
+        $this->form_validation->set_rules('address', 'Address','trim|required');
+
+        if($this->form_validation->run() == true) { 
+
+            $formArray['usersname'] = $this->input->post('usersname');
+            $formArray['f_name'] = $this->input->post('firstname');
+            $formArray['l_name'] = $this->input->post('lastname');
+            $formArray['email'] = $this->input->post('email');
+            $formArray['phone'] = $this->input->post('phone');
+            $formArray['address'] = $this->input->post('address');
+
+            $this->User_model->update($id,$formArray);
+
+            $this->session->set_flashdata('success', 'User updated successfully');
+            redirect(base_url(). 'profile/index');
+
+        } else {
+            $data['user'] = $user; 
+            $this->load->view('front/partials/header');
+            $this->load->view('front/profile', $data);
+            $this->load->view('front/partials/footer');
+        }
+    }
 }
--- application/views/front/profile.php
+++ application/views/front/profile.php
@@ -2,17 +2,24 @@
 <div class="row">
     <div class="col-md-6">
         <div class="wrapper mt-sm-5">
-            <form action="" method="POST">
+               <?php if($this->session->flashdata('success') != ""):?>
+            <div class="alert alert-success">
+                <?php echo $this->session->flashdata('success');?>
+            </div>
+            <?php endif ?>
+            <?php $loggedUser = $this->session->userdata('user');?>
+            <form action="<?php echo base_url().'profile/edit/'.$loggedUser['user_id']?>" method="POST">
                  <center>    
                          <h1><i class="fas fa-cog"></i></h1>
                 </center>
                 <h4 class="pb-4 border-bottom" style="text-align:center;">Account settings</h4>
                 <div class="py-2">
                     <div>
-                        <label for="username">Username</label>
-                        <input type="text" name="username"
-                            class="bg-light form-control <?php echo (form_error('username') != "") ? 'is-invalid' : '';?>"
+                        <label for="usersname">Usersname</label>
+                        <input type="text" name="usersname"
+                            class="bg-light form-control <?php echo (form_error('usersname') != "") ? 'is-invalid' : '';?>"
                             value="<?php echo set_value('usersname', $user['usersname']);?>">
+                            <?php echo form_error('username'); ?>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
@@ -20,12 +27,14 @@
                             <input type="text"
                                 class="bg-light form-control <?php echo (form_error('firstname') != "") ? 'is-invalid' : '';?>"
                                 name="firstname" value="<?php echo set_value('firstname', $user['f_name'])?>">
+                            <?php echo form_error('firstname'); ?>
                         </div>
                         <div class="col-md-6">
                             <label for="lastname">Last Name</label>
                             <input type="text"
                                 class="bg-light form-control <?php echo (form_error('lastname') != "") ? 'is-invalid' : '';?>"
                                 name="lastname" value="<?php echo set_value('lastname', $user['l_name'])?>">
+                             <?php echo form_error('lastname'); ?>
                         </div>
                     </div>
                     <div class="row">
@@ -34,18 +43,21 @@
                             <input type="text"
                                 class="bg-light form-control <?php echo (form_error('email') != "") ? 'is-invalid' : '';?>"
                                 name="email" value="<?php echo set_value('email', $user['email'])?>">
+                            <?php echo form_error('email'); ?>
                         </div>
                         <div class="col-md-6">
                             <label for="phone">Contact Number</label>
                             <input type="tel"
                                 class="bg-light form-control <?php echo (form_error('phone') != "") ? 'is-invalid' : '';?>"
                                 name="phone" value="<?php echo set_value('phone', $user['phone'])?>">
+                            <?php echo form_error('phone'); ?>
                         </div>
                     </div>
                     <div>
                         <label for="address">Address</label>
                         <textarea name="address" type="text" style="height:70px;"
                             class="bg-light form-control <?php echo (form_error('address') != "") ? 'is-invalid' : '';?>"><?php echo set_value('address', $user['address']);?></textarea>
+                            <?php echo form_error('address'); ?>
                     </div>
                     <div class="py-3 pb-4 border-bottom">
                         <button type="submit" class="btn btn-success mr-3">Save Changes</button>
